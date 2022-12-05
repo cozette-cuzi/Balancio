f5684d44a72dae445e87a6d997f947cefc1b5ad7
cozette-cuzi <cuzicozette@gmail.com>
2022-12-06 00:24:04 +0100

    Login and Register States

--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import { SyntheticEvent, useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log("submitted")
+  }
+
   return (
     <FormContainer>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+
+          />
         </Form.Group>
 
         <Button variant="outline-info" type="submit" className='mx-auto'>
--- frontend/src/screens/SignupScreen.tsx
+++ frontend/src/screens/SignupScreen.tsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { SyntheticEvent, useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
 
 const SignupScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log("submitted")
+  }
   return (
     <FormContainer>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="name">
           <Form.Label>Full Name</Form.Label>
-          <Form.Control type="text" placeholder="Full Name" />
+          <Form.Control type="text" placeholder="Full Name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
         </Form.Group>
 
         <Button variant="outline-info" type="submit" className='mx-auto'>
