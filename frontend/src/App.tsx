794a64a197c1dfadad500861cc3994f4f51db2ae
cozette-cuzi <cuzicozette@gmail.com>
2022-12-05 20:22:35 +0100

    initialize react project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>React app is working</h1>
+    <div>
+      <Header />
+      <h1>React is working.</h1>
+      <Footer />
+    </div>
   );
 }
 
